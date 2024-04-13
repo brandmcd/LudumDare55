52352679c4925ebbd086825fec56b80112ab7bf4
Brandon McDonald <bamcd0313@gmail.com>
2024-04-13 12:22:38 -0400

    Movement & Dialogue System

---
+++ Assets/Dialogue/DialogueAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueAsset : ScriptableObject
+{
+    
+    public string OptionalDialogueAssetName;
+    public string PersonName;
+    public string SkipToDialogueName;
+    [TextArea]
+    public string[] DialogueSet;
+}
---
+++ Assets/Dialogue/DialogueUser.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public abstract class DialogueUser : MonoBehaviour
+{
+  protected DialogueManager dialogueManager;
+   protected ScriptableObject[] assets;
+    protected string nameForDialogue;
+   
+    // Start is called before the first frame update
+  protected void Start()
+    {
+        //hook up to the dialogue manager
+        dialogueManager = FindObjectOfType<DialogueManager>();
+        //for each scriptable object check if PersonName is "", if it is set it to the name of this object
+        foreach (ScriptableObject asset in assets)
+        {
+            switch (asset.GetType())
+            {
+                case System.Type t when t == typeof(DialogueAsset):
+                    DialogueAsset dialogueAsset = (DialogueAsset)asset;
+                    if (dialogueAsset.PersonName == "")
+                    {
+                        dialogueAsset.PersonName = name;
+                    }
+                    break;
+                case System.Type t when t == typeof(ChoicesAsset):
+                    ChoicesAsset choicesAsset = (ChoicesAsset)asset;
+                    if (choicesAsset.PersonName == "")
+                    {
+                        choicesAsset.PersonName = name;
+                    }
+                    break;
+                case System.Type t when t == typeof(ImpactTextAsset):
+                    ImpactTextAsset impactTextAsset = (ImpactTextAsset)asset;
+                    if (impactTextAsset.PersonName == "")
+                    {
+                        impactTextAsset.PersonName = name;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+  protected  void OnBeginDialogue()
+    { 
+        print("begin dialogue");
+        dialogueManager.StartAssetSet(assets);
+    }
+
+}
